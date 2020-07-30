0902ac0f9aeb0c2a14daa944af88cdc7e379d4cb
Matthew Casey <mdcasey@chabloom.com>
2020-07-30 11:42:46 -0400

    Initial commit

---
+++ Payments.Account/Data/AccountDbContext.cs
@@ -0,0 +1,20 @@
+﻿// Copyright 2020 Chabloom LC. All rights reserved.
+
+using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace Payments.Account.Data
+{
+    public class AccountDbContext : DbContext
+    {
+        public AccountDbContext(DbContextOptions options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ Payments.Account/Startup.cs
@@ -0,0 +1,82 @@
+// Copyright 2020 Chabloom LC. All rights reserved.
+
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+using Payments.Account.Data;
+
+namespace Payments.Account
+{
+    public class Startup
+    {
+        private const string DevelopmentCorsName = "Development";
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<AccountDbContext>(options =>
+                options.UseSqlServer(
+                    Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddCors(options =>
+            {
+                options.AddPolicy(DevelopmentCorsName,
+                    builder =>
+                    {
+                        builder.AllowAnyOrigin()
+                            .AllowAnyHeader()
+                            .AllowAnyMethod();
+                    });
+            });
+
+            services.AddControllers();
+
+            services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "Chabloom Payments Account",
+                    Description = "Chabloom Payments Account v1 API",
+                    Version = "v1"
+                });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+
+                app.UseCors(DevelopmentCorsName);
+            }
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(options =>
+            {
+                options.SwaggerEndpoint("/swagger/v1/swagger.json", "Chabloom Payments Account v1 API");
+            });
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
+        }
+    }
+}
