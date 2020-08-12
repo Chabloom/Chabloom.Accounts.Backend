406fed8fa60f0ef331156ecae1dfa3d5f22db835
Matthew Casey <mdcasey@chabloom.com>
2020-08-11 23:07:38 -0400

    Update app component and logo

--- Payments.Account/App/src/App.tsx
+++ Payments.Account/App/src/App.tsx
@@ -1,26 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+
+import Login from "./components/Login";
+import Logout from "./components/Logout";
+import {Grid} from "@material-ui/core";
+
+import './App.scss';
+
+const App: React.FC = () => {
+    return (
+        <Router>
+            <Grid container alignItems="center" justify="center" style={{ minHeight: '100vh' }}>
+                <Switch>
+                    <Route path="/login">
+                        <Login/>
+                    </Route>
+                    <Route path="/logout">
+                        <Logout/>
+                    </Route>
+                </Switch>
+            </Grid>
+        </Router>
+    );
 }
 
 export default App;
