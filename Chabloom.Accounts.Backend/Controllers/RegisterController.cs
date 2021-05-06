109ab8819c3167e207ecc3819cebfe8c0385f156
Matthew Casey <mdcasey@chabloom.com>
2021-05-05 21:06:36 -0400

    Send email after registration

--- Chabloom.Accounts.Backend/Controllers/RegisterController.cs
+++ Chabloom.Accounts.Backend/Controllers/RegisterController.cs
@@ -3,6 +3,7 @@
 using System.Security.Claims;
 using System.Threading.Tasks;
 using Chabloom.Accounts.Backend.Data;
+using Chabloom.Accounts.Backend.Services;
 using Chabloom.Accounts.Backend.ViewModels;
 using IdentityModel;
 using Microsoft.AspNetCore.Authorization;
@@ -21,11 +22,14 @@ namespace Chabloom.Accounts.Backend.Controllers
     [Produces("application/json")]
     public class RegisterController : ControllerBase
     {
+        private readonly EmailSender _emailSender;
         private readonly ILogger<RegisterController> _logger;
         private readonly UserManager<ApplicationUser> _userManager;
 
-        public RegisterController(ILogger<RegisterController> logger, UserManager<ApplicationUser> userManager)
+        public RegisterController(EmailSender emailSender, ILogger<RegisterController> logger,
+            UserManager<ApplicationUser> userManager)
         {
+            _emailSender = emailSender;
             _logger = logger;
             _userManager = userManager;
         }
@@ -56,8 +60,7 @@ namespace Chabloom.Accounts.Backend.Controllers
             };
 
             // Create the user
-            var result = await _userManager.CreateAsync(user, viewModel.Password)
-                .ConfigureAwait(false);
+            var result = await _userManager.CreateAsync(user, viewModel.Password);
             if (!result.Succeeded)
             {
                 return BadRequest(result.Errors);
@@ -66,10 +69,18 @@ namespace Chabloom.Accounts.Backend.Controllers
             _logger.LogInformation($"User with email {viewModel.Email} registered");
 
             // Add the user name claim
-            await _userManager.AddClaimAsync(user, new Claim(JwtClaimTypes.Name, viewModel.Name))
-                .ConfigureAwait(false);
+            await _userManager.AddClaimAsync(user, new Claim(JwtClaimTypes.Name, viewModel.Name));
 
             _logger.LogInformation($"User with email {viewModel.Email} added name claim {viewModel.Name}");
+
+            var token = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+            if (!string.IsNullOrEmpty(token))
+            {
+                const string subject = "Confirm your Chabloom account";
+                await _emailSender.SendEmailAsync(user.Email, subject, token);
+
+                _logger.LogInformation($"User {user.Id} email confirmation link sent");
+            }
 
             // Return success status code
             return Ok();
